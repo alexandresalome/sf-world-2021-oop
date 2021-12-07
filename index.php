11af169e4353e74f88558253d087b4c93980cc26
Alexandre Salomé <git@pub.salome.fr>
2021-12-07 14:49:28 +0100

    Break the code

--- index.php
+++ index.php
@@ -4,8 +4,10 @@ namespace Main;
 
 use Oop\InvoiceCliRenderer;
 use Oop\InvoiceHtmlRenderer;
+use Oop\InvoiceBuilder;
 use Oop\InvoiceManager;
 use Oop\InvoiceValidatorFactory;
+use Oop\Price;
 use Symfony\Component\ErrorHandler\Debug;
 
 require_once __DIR__.'/vendor/autoload.php';
@@ -14,7 +16,19 @@ Debug::enable();
 
 // Configuration
 $discounted = ($argv[1] ?? '') === 'discount';
-$invoice = InvoiceManager::getInstance()->getFruitInvoice($discounted);
+//$invoice = InvoiceManager::getInstance()->getFruitInvoice($discounted);
+
+$invoice = (new InvoiceBuilder())
+    ->setId(123)
+    ->addLine(300, 'Apples', Price::euro(39))
+    ->removeLine('Apples')
+    ->addLine(1, 'Bananas', Price::euro(60))
+    ->increment('Bananas', 1)
+    ->addLine(1, 'Bag', Price::euro(100))
+    ->addLine(300, 'Apples', Price::euro(39))
+    ->getInvoice()
+;
+
 
 (new InvoiceValidatorFactory())
     ->getInvoiceValidator($invoice)
